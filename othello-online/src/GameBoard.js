acd15823d7efab7f068ed623d652b7894a5f6c09
Ian Zanger <izanger@purdue.edu>
2018-04-20 13:19:01 -0400

    Add syncing in GameBoard

--- othello-online/src/GameBoard.js
+++ othello-online/src/GameBoard.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import rebase from './rebase.js'
 
 class GameBoard extends Component {
+    constructor(){
+        super();
+        this.state = {
+            board: { },
+            boardSynced: false,
+        }
+    }
+    componentWillMount = () => {
+        //rebase.syncState(`games/${this.props.gameID}/board`, {
+        rebase.syncState(`games/testingID/board`, {
+            context: this,
+            state: 'board',
+            then(data){
+                let newState = { ...this.state }
+                newState.boardSynced = true
+                this.setState(newState)
+            }
+        })
+    }
     render = () => {
-        return (
-            <p> ahoy </p>
-        )
+        if(this.state.boardSynced){
+            return (
+                <p> ahoy it's synced: {this.state.board.testvalue}</p>
+            )
+        } else {
+            return (
+                <p> ahoy it's not synced</p>
+            )
+        }
+
     }
 }
 export default GameBoard
