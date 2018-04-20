fa6183345d91e098b893994c4d5929b7999a6eb7
Ian Zanger <izanger@purdue.edu>
2018-04-20 13:05:45 -0400

    Add GameBoard component

---
+++ othello-online/src/GameBoard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import rebase from './rebase.js'
+
+class GameBoard extends Component {
+    render = () => {
+        return (
+            <p> ahoy </p>
+        )
+    }
+}
+export default GameBoard
