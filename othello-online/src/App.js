319654a97031b07676a19bbf1ff0f6411fec4fc1
Ian Zanger <izanger@purdue.edu>
2018-04-20 16:58:25 -0400

    Add game logic

--- othello-online/src/App.js
+++ othello-online/src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
           if (!isObjectEmpty(this.state.user) ){
             console.log(this.state.user)
             console.log(this.state.user.uid)
-            return <Home playerID={this.state.user.uid} getAppState={this.getAppState} setAppState={this.setAppState} goToUrl={this.goToUrl} />
+            //return <Home playerID={this.state.user.uid} getAppState={this.getAppState} setAppState={this.setAppState} goToUrl={this.goToUrl} />
             return <GameBoard playerID="abcd"/>
           }
           else {
